a5fbc40a211b5d02db1c7f3d65703797275f793f
jakesummer <jakersummer@gmail.com>
2026-03-18 17:18:56 -0400

    Complete foundations lesson 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    let output = [];
+
+    for (item of arr) {
+        if (!itemsToRemove.includes(item)) {
+            output.push(item);
+        }
+    }
+
+    return output;
 };
 
 // Do not edit below this line
