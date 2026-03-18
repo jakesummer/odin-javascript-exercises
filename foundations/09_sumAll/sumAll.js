863bc1afd8d3edcf76b888698fe670da2eb44102
jakesummer <jakersummer@gmail.com>
2026-03-18 17:29:12 -0400

    Complete foundations lesson 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (
+        a < 0 || 
+        b < 0 || 
+        typeof a !== "number" || 
+        typeof b !== "number" || 
+        !Number.isInteger(a) || 
+        !Number.isInteger(b)
+    ) {
+        return "ERROR";
+    }
 
+    let lower;
+    let upper;
+
+    if (a > b) {
+        lower = b;
+        upper = a;
+    } else {
+        lower = a;
+        upper = b;
+    }
+
+    let sum = 0;
+
+    for (let i = lower; i <= upper; i++) {
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
