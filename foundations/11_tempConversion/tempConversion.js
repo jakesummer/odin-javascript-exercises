dba00b66f0de733e4abce5def83250c12bec9b71
jakesummer <jakersummer@gmail.com>
2026-03-18 17:57:56 -0400

    Complete foundations lesson 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = ((5/9) * (temp - 32));
+
+  if (Number.isInteger(newTemp)) {
+    return newTemp;
+  }
+  return Number(newTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = ((temp * 1.8) + 32);
+
+  if (Number.isInteger(newTemp)) {
+    return newTemp;
+  }
+  return Number(newTemp.toFixed(1));
 };
 
 // Do not edit below this line
