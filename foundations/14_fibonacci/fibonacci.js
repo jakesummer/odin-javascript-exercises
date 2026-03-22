86712086a77c3d64777003089b11d9d1adae1082
jakesummer <jakersummer@gmail.com>
2026-03-21 22:11:20 -0400

    Complete foundations lesson 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let sequence = [0, 1];
 
+    if (num < 0) {
+        return "OOPS";
+    } else if (num <= 1) {
+        return sequence[num];
+    }
+
+    for (let i = 2; i <= num; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+
+    return sequence[num];
 };
 
 // Do not edit below this line
