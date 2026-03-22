705e2c95caa7023cf27e6a8a794649a7a413bd16
jakesummer <jakersummer@gmail.com>
2026-03-21 22:23:40 -0400

    Complete foundations lesson 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = (new Date()).getFullYear();
+    return arr.sort((a, b) => {
+        let aAge = ("yearOfDeath" in a) ? (a.yearOfDeath - a.yearOfBirth) : (currentYear - a.yearOfBirth);
+        let bAge = ("yearOfDeath" in b) ? (b.yearOfDeath - b.yearOfBirth) : (currentYear - b.yearOfBirth);
+        return bAge - aAge;
+    })[0];
 };
 
 // Do not edit below this line
