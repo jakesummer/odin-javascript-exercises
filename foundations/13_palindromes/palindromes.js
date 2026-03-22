a81420baa628005d77d3442876bd1f381ab7e9a2
jakesummer <jakersummer@gmail.com>
2026-03-21 22:03:53 -0400

    Complete foundations lesson 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const lettersNumbers = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    str = str.
+        toLowerCase().
+        split("").
+        filter((char) => lettersNumbers.includes(char)).
+        join("");
+
+    let reversed = str.
+        split("").
+        reverse().
+        join("");
+
+    return reversed === str; 
 };
 
 // Do not edit below this line
